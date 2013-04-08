84212ccbf0ce5f2984e8ba50a3bdadf0480353b0
Tami Forrester <tforrest@mit.edu>
2013-04-08 00:01:52 -0400

    added login and dashboard screen

--- HuntersAndZombies/gen/com/example/huntersandzombiesdemo/R.java
+++ HuntersAndZombies/gen/com/example/huntersandzombiesdemo/R.java
@@ -21,21 +21,75 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     public static final class color {
         public static final int black_overlay=0x7f040000;
     }
+    public static final class dimen {
+        /**  Default screen margins, per the Android Design guidelines. 
+
+         Customize dimensions originally defined in res/values/dimens.xml (such as
+         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
+    
+         */
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int dummy_button=0x7f070002;
-        public static final int fullscreen_content=0x7f070000;
-        public static final int fullscreen_content_controls=0x7f070001;
+        public static final int action_forgot_password=0x7f09000f;
+        public static final int action_settings=0x7f09000e;
+        public static final int buttonBar=0x7f090000;
+        public static final int duelButton=0x7f090002;
+        public static final int dummy_button=0x7f090006;
+        public static final int email=0x7f09000a;
+        public static final int fullscreen_content=0x7f090004;
+        public static final int fullscreen_content_controls=0x7f090005;
+        public static final int inventoryButton=0x7f090003;
+        public static final int login=0x7f09000c;
+        public static final int login_form=0x7f090009;
+        public static final int login_status=0x7f090007;
+        public static final int login_status_message=0x7f090008;
+        public static final int password=0x7f09000b;
+        public static final int scoreButton=0x7f090001;
+        public static final int sign_in_button=0x7f09000d;
     }
     public static final class layout {
-        public static final int activity_fullscreen=0x7f030000;
+        public static final int activity_dashboard=0x7f030000;
+        public static final int activity_duel=0x7f030001;
+        public static final int activity_fullscreen=0x7f030002;
+        public static final int activity_login=0x7f030003;
+        public static final int activity_score=0x7f030004;
+    }
+    public static final class menu {
+        public static final int dashboard=0x7f080000;
+        public static final int duel=0x7f080001;
+        public static final int login=0x7f080002;
+        public static final int score=0x7f080003;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int dummy_button=0x7f050001;
-        public static final int dummy_content=0x7f050002;
+        public static final int action_forgot_password=0x7f060010;
+        public static final int action_settings=0x7f060004;
+        public static final int action_sign_in_register=0x7f06000e;
+        public static final int action_sign_in_short=0x7f06000f;
+        public static final int app_name=0x7f060000;
+        public static final int duel=0x7f060007;
+        public static final int dummy_button=0x7f060001;
+        public static final int dummy_content=0x7f060002;
+        public static final int error_field_required=0x7f060015;
+        public static final int error_incorrect_password=0x7f060014;
+        public static final int error_invalid_email=0x7f060012;
+        public static final int error_invalid_password=0x7f060013;
+        public static final int hello_world=0x7f060005;
+        public static final int login_progress_signing_in=0x7f060011;
+        /**  Strings related to login 
+         */
+        public static final int prompt_email=0x7f06000c;
+        public static final int prompt_password=0x7f06000d;
+        public static final int score=0x7f060008;
+        public static final int shop_inventory=0x7f060006;
+        public static final int title_activity_dashboard=0x7f060003;
+        public static final int title_activity_duel=0x7f06000a;
+        public static final int title_activity_login=0x7f06000b;
+        public static final int title_activity_score=0x7f060009;
     }
     public static final class style {
         /** 
@@ -59,15 +113,16 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
-        public static final int ButtonBar=0x7f060003;
-        public static final int ButtonBarButton=0x7f060004;
-        public static final int FullscreenActionBarStyle=0x7f060005;
-        public static final int FullscreenTheme=0x7f060002;
+        public static final int AppTheme=0x7f070001;
+        public static final int ButtonBar=0x7f070003;
+        public static final int ButtonBarButton=0x7f070004;
+        public static final int FullscreenActionBarStyle=0x7f070006;
+        public static final int FullscreenTheme=0x7f070002;
+        public static final int LoginFormContainer=0x7f070005;
     }
     public static final class styleable {
         /** 
