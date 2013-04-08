dfedfb1513a41cbad5640018b35bb690413bd851
Tami Forrester <tforrest@mit.edu>
2013-04-08 18:02:38 -0400

    added activities score and duel, made dashboard the starting screen

--- HuntersAndZombies/src/com/example/huntersandzombiesdemo/Dashboard.java
+++ HuntersAndZombies/src/com/example/huntersandzombiesdemo/Dashboard.java
@@ -1,8 +1,10 @@
 package com.example.huntersandzombiesdemo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
 
 public class Dashboard extends Activity {
@@ -20,8 +22,48 @@ public class Dashboard extends Activity {
 		inventoryButton = (Button) findViewById(R.id.inventoryButton);
 		//add event handlers to change view
 		
+		scoreButton.setOnClickListener(scoreHandler);
+		inventoryButton.setOnClickListener(inventoryHandler);
+		duelButton.setOnClickListener(duelHandler);
 //		scoreButton.setOnClickListener(this);
 	}
+	/*
+	 * on click listener for the scorebutton
+	 */
+	View.OnClickListener scoreHandler = new View.OnClickListener() {		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent intent = new Intent(Dashboard.this, Score.class);
+			startActivity(intent);
+		}
+	};
+	
+	/**
+	 * On click listener for the inventory button
+	 */
+	View.OnClickListener inventoryHandler = new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+//			Intent intent = new Intent(this, ...intent..);
+//			startActivity(intent);
+		}
+	};
+	
+	/**
+	 * On click listener for the duel handler button
+	 */
+	View.OnClickListener duelHandler = new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			Intent intent = new Intent(Dashboard.this, Duel.class);
+			startActivity(intent);
+		}
+	};
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
